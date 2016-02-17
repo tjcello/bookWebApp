3a45f5267da89f48957eb8d09b44c0c5875a5aca
tjcel <tjcel@10.30.21.135>
2016-02-17 16:56:10 -0600

    pushing all code to github for first time

---
+++ src/main/java/edu/wctc/tjd/bookwebapp/model/AuthorService.java
@@ -0,0 +1,22 @@
+package edu.wctc.tjd.bookwebapp.model;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class AuthorService {
+   private AuthorDaoStrategy dao = new AuthorDao();
+   
+   public List<Author> getAuthorList() throws ClassNotFoundException, SQLException{
+       return dao.getAuthorList();
+       
+   }
+   
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        AuthorService srv = new AuthorService();
+        List<Author> authors = srv.getAuthorList();
+        System.out.println(authors);
+    }
+}
+
